22f8d868748cfbdc84ca37a0eaea2d938b824cac
shubham12-prof <shubhsemwal12@gmail.com>
2025-04-17 11:55:19 +0530

    added id in property

--- src/app/components/section/propertyGallary/page.js
+++ src/app/components/section/propertyGallary/page.js
@@ -27,28 +27,28 @@ const PropertyGallery = () => {
       title: "Modern Office Space",
     },
     {
-      id: "plot-1",
+      id: "plot-2",
       image:
         "https://res.cloudinary.com/dmj6ur8sm/image/upload/v1744829153/sylon71yhel1hqgxpz0j.jpg",
       title: "Premium Plot in Saharanpur",
     },
     {
-      id: "home-1",
+      id: "home-2",
       image:
         "https://res.cloudinary.com/dmj6ur8sm/image/upload/v1744829154/hmzph11yv4cn8w0xtrih.jpg",
       title: "Elegant Family Home",
     },
     {
-      id: "office-1",
+      id: "office-2",
       image:
         "https://res.cloudinary.com/dmj6ur8sm/image/upload/v1744829154/oufhnwfnee7bsrsa39vl.jpg",
       title: "Modern Office Space",
     },
   ];
 
-  // const handleClick = (id) => {
-  //   router.push(`/property/${id}`);
-  // };
+  const handleClick = (id) => {
+    router.push(`/property/${id}`);
+  };
 
   return (
     <section className="bg-white dark:bg-gray-900 py-12 px-4 md:px-8 lg:px-16">
@@ -62,24 +62,24 @@ const PropertyGallery = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-6xl mx-auto">
-        {/* {properties.map((property) => ( */}
-        <div
-          // key={property.id}
-          // onClick={() => handleClick(property.id)}
-          className="cursor-pointer group relative overflow-hidden rounded-xl shadow-lg hover:shadow-2xl transition duration-300"
-        >
-          <Image
-            src={property.image}
-            alt={property.title}
-            className="w-full h-64 object-cover transform group-hover:scale-105 transition duration-300"
-          />
-          {/* <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 text-center text-lg font-semibold">
+        {properties.map((property) => (
+          <div
+            key={property.id}
+            onClick={() => handleClick(property.id)}
+            className="cursor-pointer group relative overflow-hidden rounded-xl shadow-lg hover:shadow-2xl transition duration-300"
+          >
+            <Image
+              src={property.image}
+              alt={property.title}
+              width={400}
+              height={300}
+              className="w-full h-64 object-cover transform group-hover:scale-105 transition duration-300"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 text-center text-lg font-semibold">
               {property.title}
-            </div> */}
-        </div>
-        {/* ) */}
-        {/* ) */}
-        {/* } */}
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
